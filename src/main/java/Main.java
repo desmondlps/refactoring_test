e8221a19fd2ed0867ed41e4e9d3a501bc668626f
desmond <desmond.lee@olea.net>
2022-05-25 13:54:28 +0800

    add question with smells

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,28 @@
 public class Main {
     public static void main(String[] args) {
+
+        double totalmoneychangerprofit = 0;
+        MoneyChanger moneyChanger = new MoneyChanger();
+
+        double tx1 = moneyChanger.ExchangeSGD(200, "EUR");
+        System.out.println("Exchanging $200 SGD, Gives me $" + tx1 + " EUR back");
+        totalmoneychangerprofit += moneyChanger.currentTransactionProfit;
+
+        double tx2 = moneyChanger.ExchangeSGD(200, "MYR");
+        System.out.println("Exchanging $200 SGD, Gives me $" + tx2 + " MYR back");
+        totalmoneychangerprofit += moneyChanger.currentTransactionProfit;
+
+        double tx3 = moneyChanger.ExchangeSGD(200, "USD");
+        System.out.println("Exchanging $200 SGD, Gives me $" + tx3 + " USD back");
+        totalmoneychangerprofit += moneyChanger.currentTransactionProfit;
+
+        double tx4 = moneyChanger.ExchangeSGD(200, "JPY");
+        System.out.println("Exchanging $200 SGD, Gives me $" + tx4 + " JPY back");
+        totalmoneychangerprofit += moneyChanger.currentTransactionProfit;
+
+        double moneyChangerProfitAfterPayingRent = totalmoneychangerprofit - moneyChanger.rental_fee_per_day;
+
+        System.out.println("Money changer total profit at the end of day is $" + totalmoneychangerprofit);
+        System.out.println("Money changer total profit at the end of day after paying rent is $" + moneyChangerProfitAfterPayingRent);
     }
 }
---
+++ src/main/java/MoneyChanger.java
@@ -0,0 +1,36 @@
+public class MoneyChanger {
+
+    public double rental_fee_per_day = 1000;
+    public double currentTransactionProfit = 0;
+
+    public double ExchangeSGD(double sgd, String toCurrency) {
+        double exchange = 0;
+
+        if (toCurrency == "USD") {
+            exchange = sgd * 0.73;
+            currentTransactionProfit = exchange * 0.1;
+        }
+        if (toCurrency == "MYR") {
+            exchange = sgd * 3.1;
+            currentTransactionProfit = exchange * 0.1;
+        }
+        if (toCurrency == "IND") {
+            exchange = sgd * 56.42;
+            currentTransactionProfit = exchange * 0.1;
+        }
+        if (toCurrency == "AUD") {
+            exchange = sgd * 1.02;
+            currentTransactionProfit = exchange * 0.1;
+        }
+        if (toCurrency == "CND") {
+            exchange = sgd * 0.93;
+            currentTransactionProfit = exchange * 0.1;
+        }
+        if (toCurrency == "EUR") {
+            exchange = sgd * 0.68;
+            currentTransactionProfit = exchange * 0.1;
+        }
+
+        return exchange - currentTransactionProfit;
+    }
+}
